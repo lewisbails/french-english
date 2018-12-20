2c65cb5339cf9c9603fb339e347a7eb27c2b1a73
Lewis Bails <lewis.bails@gmail.com>
2018-12-20 15:04:14 +0100

    pretrained embeddings glove works

--- seq2seq_gru.py
+++ seq2seq_gru.py
@@ -9,11 +9,13 @@ from __future__ import print_function
 
 from keras.models import Model, load_model
 from keras.layers import Input, GRU, Dense, Embedding
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import re
 
 batch_size = 64  # Batch size for training.
-epochs = 2  # Number of epochs to train for.
+epochs = 1  # Number of epochs to train for.
 latent_dim = 512  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
@@ -29,7 +31,7 @@ target_words_freq = {}
 with open(data_path,'r',encoding='utf-8') as f:
     lines = f.read().split('\n')
 for line in lines[: min(num_samples,len(lines)-1)]:
-    input_text,target_text = line.split('\t')
+    input_text,target_text = line.lower().split('\t')
     target_text = '<START> '+target_text+' <END>'
     input_text = re.sub('([.,!?()])', r' \1 ', input_text)
     input_text = re.sub('\s{2,}', ' ', input_text)
@@ -49,68 +51,36 @@ for line in lines[: min(num_samples,len(lines)-1)]:
                 target_words_freq[word]=1
         else:
             target_words_freq[word]+=1 
+        
             
 input_replace = set()
 target_replace = set()
 for key,value in input_words_freq.copy().items():
-    if value<2:
+    if value<1:
         del input_words_freq[key]
         input_replace.add(key)
         
 for key,value in target_words_freq.copy().items():
-    if value<2:
+    if value<1:
         del target_words_freq[key]
         target_replace.add(key)
         
 
-# Now we know what words to replace with <UNKNOWN> we can iterate the text again
-   
-input_words_freq['<UNKNOWN>']=0
-target_words_freq['<UNKNOWN>']=0     
-
-input_words = sorted(list(input_words_freq.keys()))
-target_words = sorted(list(target_words_freq.keys()))
+# get lists of words including pad and unknown tokens
+input_words = ['<PAD>']+['<UNKNOWN>']+sorted(list(input_words_freq.keys()))
+target_words = ['<PAD>']+['<UNKNOWN>']+sorted(list(target_words_freq.keys()))
 num_encoder_tokens = len(input_words)
 num_decoder_tokens = len(target_words)
 max_encoder_seq_length = max([len(txt.split(' ')) for txt in input_texts])
-max_decoder_seq_length = max([len(txt.split(' ')) for txt in target_texts])   
-
-# For using 1-hot as input for each word
-#input_token_index = dict(
-#        [(word,i) for i,word in enumerate(input_words)])
-#target_token_index = dict(
-#        [(word,i) for i,word in enumerate(target_words)])
-#
-#encoder_input_data = np.zeros(
-#        (len(input_texts),max_encoder_seq_length,num_encoder_tokens),
-#        dtype='float64')
-#decoder_input_data = np.zeros(
-#        (len(input_texts),max_decoder_seq_length,num_decoder_tokens),
-#        dtype='float64')
-#decoder_target_data = np.zeros(
-#        (len(input_texts),max_decoder_seq_length,num_decoder_tokens),
-#        dtype='float64')
-#
-#for i,(input_text,target_text) in enumerate(zip(input_texts,target_texts)):
-#    for t,word in enumerate(input_text.split(' ')):
-#        if word in input_replace:
-#            word = '<UNKNOWN>'
-#        encoder_input_data[i,t,input_token_index[word]]=1
-#    for t,word in enumerate(target_text.split(' ')):
-#        if word in target_replace:
-#            word = '<UNKNOWN>'
-#        decoder_input_data[i,t,target_token_index[word]]=1
-#        if t>0:
-#            decoder_target_data[i,t-1,target_token_index[word]]=1
-            
+max_decoder_seq_length = max([len(txt.split(' ')) for txt in target_texts])            
 
 #For using embedded vectors for each word
+#This section turns texts into integer vectors with padding to max sentence length
+
 input_token_index = dict(
-        [(word,i+1) for i,word in enumerate(input_words)])
-input_token_index['<PAD>']=0
+        [(word,i) for i,word in enumerate(input_words)])
 target_token_index = dict(
-        [(word,i+1) for i,word in enumerate(target_words)])
-target_token_index['<PAD>']=0
+        [(word,i) for i,word in enumerate(target_words)])
 
 encoder_input_data = np.zeros(
         (len(input_texts),max_encoder_seq_length),
@@ -120,7 +90,7 @@ decoder_input_data = np.zeros(
         dtype='float64')
 decoder_target_data = np.zeros(
         (len(input_texts),max_decoder_seq_length,num_decoder_tokens),
-        dtype='float64')
+        dtype='float32')
 
 for i,(input_text,target_text) in enumerate(zip(input_texts,target_texts)):
     for t,word in enumerate(input_text.split(' ')):
@@ -134,10 +104,29 @@ for i,(input_text,target_text) in enumerate(zip(input_texts,target_texts)):
         if t>0:
             decoder_target_data[i,t-1,target_token_index[word]]=1
             
+# Pre-trained embeddings
+embeddings_index = dict()
+f = open('/glove6b/glove.6B.100d.txt', encoding="utf8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    if word in input_words:
+        coefs = np.asarray(values[1:], dtype='float64')
+        embeddings_index[word] = coefs
+        if len(embeddings_index)==num_encoder_tokens:
+            break
+f.close()
+print('Loaded %s word vectors.' % len(embeddings_index))
+# create a weight matrix for words in training docs
+embedding_matrix = np.zeros((num_encoder_tokens, 100))
+for word, i in input_token_index.items():
+	embedding_vector = embeddings_index.get(word)
+	if embedding_vector is not None:
+		embedding_matrix[i] = embedding_vector
+            
 # GRU Encoder
 encoder_inputs = Input(shape=(None,))
-x = Embedding(num_encoder_tokens, latent_dim)(encoder_inputs)
-#encoder_inputs = Input(shape=(None,num_encoder_tokens))
+x = Embedding(num_encoder_tokens, 100, weights=[embedding_matrix], trainable=True)(encoder_inputs)
 encoder_gru = GRU(latent_dim,return_state=True)
 encoder_outputs, encoder_state = encoder_gru(x)
 
@@ -170,7 +159,7 @@ model.save('s2s.h5')
 encoder_model = Model(encoder_inputs,encoder_state)
 
 decoder_state_input = Input(shape=(latent_dim,))
-decoder_outputs,decoder_state = decoder_gru(decoder_inputs,
+decoder_outputs,decoder_state = decoder_gru(x,
                                             initial_state=decoder_state_input)
 decoder_outputs = decoder_dense(decoder_outputs)
 decoder_model = Model([decoder_inputs]+[decoder_state_input],
@@ -185,13 +174,12 @@ reverse_target_word_index = dict(
 def decode_sequence(input_seq):
     states_value = encoder_model.predict(input_seq)
     
-    target_seq = np.zeros((1,1,num_decoder_tokens))
-    target_seq[0,0,target_token_index['<START>']] = 1
+    decoder_input_seq = [target_token_index['<START>']]
     
     stop_condition = False
-    decoded_sentence = ''
+    decoded_sentence = []
     while not stop_condition:
-        output_tokens,states_value = decoder_model.predict([target_seq]+[states_value])
+        output_tokens,states_value = decoder_model.predict([decoder_input_seq]+[states_value])
         
         sample_token_index = np.argmax(output_tokens[0,-1,:])
         
@@ -200,13 +188,16 @@ def decode_sequence(input_seq):
         if (sampled_word == '<END>' or len(decoded_sentence) > max_decoder_seq_length):
             stop_condition = True
         elif (sampled_word!='<PAD>'):
-            decoded_sentence+=' '
-            decoded_sentence+=sampled_word
+            decoded_sentence.append(' ')
+            decoded_sentence.append(sampled_word)
         
-        target_seq = np.zeros((1,1,num_decoder_tokens))
-        target_seq[0,0,sample_token_index]=1
+        decoder_input_seq=[target_token_index[sampled_word]]
+    
+    decoded_string=''
+    for word in decoded_sentence:
+        decoded_string+=word
         
-    return decoded_sentence
+    return decoded_string
 
 
 
